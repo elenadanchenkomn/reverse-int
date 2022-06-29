6e4c2ebda42e89f37be9b6160935452a5cbd5c1b
elenadanchenkomn <elenadanchenkomn@gmail.com>
2022-06-29 23:33:30 +0200

    feat: created function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+ let result = '';
+ if (typeof n === 'number') {
+    n = Math.abs(n)
+ }
+ n = n.toString();
+
+ for (let i = n.length - 1; i !== -1; i--) {
+    result = result + n[i]
+
+ }
+
+  return result
 }
